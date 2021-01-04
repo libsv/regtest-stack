e2b5ec6e6acac810b8ccbefa9870c4828e991524
Mark Robson <m.robson@8-LM-00154.local>
2021-01-04 12:57:32 +0000

    tweak: correcting regtest container name

--- regtest.sh
+++ regtest.sh
@@ -1,1 +1,1 @@
-docker exec regtest-stack_sv_1 /opt/bitcoin-sv/bitcoin-sv-1.0.6/bin/bitcoin-cli -rpcuser=rpc -rpcpassword=rpc $@
+docker exec sv /opt/bitcoin-sv/bitcoin-sv-1.0.6/bin/bitcoin-cli -rpcuser=rpc -rpcpassword=rpc $@
