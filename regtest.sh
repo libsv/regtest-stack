1a563f256ca708a9a1c978a644edb87aae5864ad
Jad Wahab <j.wahab@nchain.com>
2021-02-18 12:36:32 +0000

    upgrade to 1.0.7 (add libatomic1)

--- regtest.sh
+++ regtest.sh
@@ -1,1 +1,1 @@
-docker exec node1 /opt/bitcoin-sv/bitcoin-sv-1.0.6/bin/bitcoin-cli -rpcuser=bitcoin -rpcpassword=bitcoin $@
+docker exec node1 /opt/bitcoin-sv/bitcoin-sv-1.0.7/bin/bitcoin-cli -rpcuser=bitcoin -rpcpassword=bitcoin $@
